a1e1e4b3b2b2e6ceb25925955ad9261c243bd942
Mateusz de <s24549@pjwstk.edu.pl>
2021-11-17 23:07:03 +0100

    Changes to be committed: 	modified:   src/s03-echo.cpp

--- education-introduction-to-programming-cxx/src/s03-echo.cpp
+++ education-introduction-to-programming-cxx/src/s03-echo.cpp
@@ -9,17 +9,23 @@ std::string lineFlag    = "-l";
 std::string reverseFlag = "-r";
 std::string toUpperFlag = "-u";
 std::string noEndl      = "-n";
+int itr                 = 0;
 
-auto main() -> int
+// auto main() -> int
+
+auto main(int argc, char* argv[]) -> int
 {
+    auto args = std::vector<std::string>{};
+    std::copy_n(argv, argc, std::back_inserter(args));
+
     auto userInput = std::string{};
-    std::cout << "Enter a word: ";
-    std::getline(std::cin, userInput);
+    userInput      = argv[1];
 
     int const a = userInput.length();
 
     std::vector<char> charVector(a);
     std::copy(userInput.begin(), userInput.end(), charVector.begin());
+
 
     for (int i = 0; i <= a; i++) {
         std::cout << charVector[i] << " ";
@@ -27,60 +33,65 @@ auto main() -> int
     // std::cout << std::endl;  //zakomentowalem std::endl zeby na koncu
     // programu wykorzystac flage noEndl "-n"
 
-    std::size_t foundLineFlag = userInput.find(lineFlag);
+    for (auto const& eachString : args) {
+        itr++;
 
-    if (foundLineFlag != std::string::npos) {
-        std::cout << std::endl;  // dodaje std::endl zeby lineFlag wykonac od
-                                 // nowej linii, pomimo tego i pomimo ponizszego
-                                 // "if" przy fladze "-l" na koncu dostaje
-                                 // dodatkowy std::endl - nie wiem skad sie
-                                 // bierze
-        for (int i = 0; i <= a; i++) {
-            if (i < (a - 1)) {
-                std::cout << charVector[i];
-                std::cout << std::endl;
-            } else {
-                std::cout << charVector[i];
+        std::size_t foundLineFlag = eachString.find(lineFlag);
+
+        if (foundLineFlag != std::string::npos) {
+            std::cout << std::endl;  // dodaje std::endl zeby lineFlag wykonac
+                                     // od nowej linii, pomimo tego i pomimo
+                                     // ponizszego "if" przy fladze "-l" na
+                                     // koncu dostaje dodatkowy std::endl - nie
+                                     // wiem skad sie bierze
+            for (int i = 0; i <= a; i++) {
+                if (i < (a - 1)) {
+                    std::cout << charVector[i] << "\n";
+                    std::cout << std::endl;
+                } else {
+                    std::cout << charVector[i];
+                }
             }
         }
-    }
 
-    std::size_t foundToUpperFlag = userInput.find(toUpperFlag);
+        std::size_t foundToUpperFlag = eachString.find(toUpperFlag);
 
-    if (foundToUpperFlag != std::string::npos) {
-        std::cout << std::endl;  // dodaje std::endl zeby toUpperFlag wykonac od
-                                 // nowej linii
+        if (foundToUpperFlag != std::string::npos) {
+            std::cout << std::endl;  // dodaje std::endl zeby toUpperFlag
+                                     // wykonac od nowej linii
 
-        for (int i = 0; i <= a; i++) {
-            charVector[i] = toupper(charVector[i]);
-            std::cout << charVector[i];
-            std::cout << " ";
+            for (int i = 0; i <= a; i++) {
+                charVector[i] = toupper(charVector[i]);
+                std::cout << charVector[i];
+                std::cout << " ";
+            }
+
+            for (int i = 0; i <= a; i++) {
+                charVector[i] = tolower(charVector[i]);  // powracam do malych
+                                                         // liter w charVector
+            }
         }
 
-        for (int i = 0; i <= a; i++) {
-            charVector[i] = tolower(charVector[i]);
+        std::size_t foundReverseFlag = eachString.find(reverseFlag);
+
+        if (foundReverseFlag != std::string::npos) {
+            std::reverse(charVector.begin(), charVector.end());
+
+            std::cout << std::endl;  // dodaje std::endl zeby reverseFlag
+                                     // wykonac od nowej linii
+
+            for (int i = 0; i <= a; i++) {
+                std::cout << charVector[i];
+                std::cout << " ";
+            }
+            std::reverse(charVector.begin(),
+                         charVector.end());  // powracam do pierotnej kolejnosci
+                                             // liter w charVector
         }
-    }
 
-    std::size_t foundReverseFlag = userInput.find(reverseFlag);
-
-    if (foundReverseFlag != std::string::npos) {
-        std::reverse(charVector.begin(), charVector.end());
-
-        std::cout << std::endl;  // dodaje std::endl zeby reverseFlag wykonac od
-                                 // nowej linii
-
-        for (int i = 0; i <= a; i++) {
-            std::cout << charVector[i];
-            std::cout << " ";
+        if (eachString != noEndl && itr == (int)args.size()) {
+            std::cout << "\n";
         }
-    }
-
-    std::size_t foundNoEndl = userInput.find(noEndl);
-
-    if (foundNoEndl != std::string::npos) {
-    } else {
-        std::cout << std::endl;
     }
 
     return 0;

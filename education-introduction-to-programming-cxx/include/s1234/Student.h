7ade179ebf4f3a4a2461ae30765783465fbb77ff
Mateusz de <s24549@pjwstk.edu.pl>
2021-11-29 19:34:05 +0100

    Changes to be committed: 	modified:   include/s1234/Student.h 	modified:   src/s03-Student.cpp

--- education-introduction-to-programming-cxx/include/s1234/Student.h
+++ education-introduction-to-programming-cxx/include/s1234/Student.h
@@ -2,17 +2,17 @@
 #include <string>
 
 struct Student {
+    std::string const imie;
+    std::string const nazwisko;
+    std::string const nr_indeksu;
+    int aktualny_semestr;
+    double srednia_ocen;
 
-	std::string const imie;
-	std::string const nazwisko;
-	std::string const nr_indeksu;
-	int aktualny_semestr;
-	double srednia_ocen;
+    auto to_string() const -> std::string;
 
-	auto to_string() const -> std::string;
-
-	Student(std::string imie, std::string nazwisko, std::string nr_indeksu, int aktualny_semestr, double srednia_ocen);
-
-	
-
+    Student(std::string imie,
+            std::string nazwisko,
+            std::string nr_indeksu,
+            int aktualny_semestr,
+            double srednia_ocen);
 };
--- education-introduction-to-programming-cxx/src/s03-Student.cpp
+++ education-introduction-to-programming-cxx/src/s03-Student.cpp
@@ -1,24 +1,34 @@
-#include <istream>
-#include <string>
 #include <s1234/Student.h>
 
-Student::Student(std::string imie, std::string nazwisko, std::string nr_indeksu, int aktualny_semestr, double srednia_ocen)
-	: imie{imie}, nazwisko{nazwisko}, nr_indeksu{nr_indeksu}, aktualny_semestr{aktualny_semestr}, srednia_ocen{srednia_ocen} {}
+#include <istream>
+#include <string>
+
+Student::Student(std::string imie,
+                 std::string nazwisko,
+                 std::string nr_indeksu,
+                 int aktualny_semestr,
+                 double srednia_ocen)
+        : imie{imie}
+        , nazwisko{nazwisko}
+        , nr_indeksu{nr_indeksu}
+        , aktualny_semestr{aktualny_semestr}
+        , srednia_ocen{srednia_ocen}
+{}
 
 auto Student::to_string() const -> std::string
 {
-	std::string str_aktualny_semestr = std::to_string(aktualny_semestr);
-	std::string str_srednia_ocen = std::to_string(srednia_ocen);
+    std::string str_aktualny_semestr = std::to_string(aktualny_semestr);
+    std::string str_srednia_ocen     = std::to_string(srednia_ocen);
 
-	return (imie + ", " + nazwisko + ", " + nr_indeksu + ", sem: " + str_aktualny_semestr + ", " + str_srednia_ocen);
-
+    return (imie + ", " + nazwisko + ", " + nr_indeksu
+            + ", sem: " + str_aktualny_semestr + ", " + str_srednia_ocen);
 }
 
 auto main() -> int
 {
-	auto Jan_Kowalski = Student{"Jan", "Kowalski", "s12345", 1, 4.25};
-	
-	std::cout << Jan_Kowalski.to_string() << std::endl;
+    auto Jan_Kowalski = Student{"Jan", "Kowalski", "s12345", 1, 4.25};
 
-	return 0;
+    std::cout << Jan_Kowalski.to_string() << std::endl;
+
+    return 0;
 }

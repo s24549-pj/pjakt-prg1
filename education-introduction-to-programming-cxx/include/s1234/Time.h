e12ace7805f2fc9b412c7775b2d409c3915d5a44
Mateusz de <s24549@pjwstk.edu.pl>
2021-11-29 22:39:53 +0100

    Changes to be committed: 	modified:   include/s1234/Time.h 	modified:   src/s03-Time.cpp

--- education-introduction-to-programming-cxx/include/s1234/Time.h
+++ education-introduction-to-programming-cxx/include/s1234/Time.h
@@ -2,16 +2,26 @@
 #include <string>
 
 struct Time {
+    int hour;
+    int minute;
+    int second;
 
-	int hour;
-	int minute;
-	int second;
+    enum class Time_of_day {
 
-	auto to_string() const -> std::string;
-	auto next_hour() -> void;
-	auto next_minute() -> void;
-	auto next_second() -> void;
+        morning,
+        day,
+        evening,
+        night
 
-	Time(int hour, int minute, int second);
+    };
 
+    auto to_string() const -> std::string;
+    auto next_hour() -> void;
+    auto next_minute() -> void;
+    auto next_second() -> void;
+
+    auto time_of_day() const -> Time_of_day;
+    auto to_string(Time_of_day) -> std::string;
+
+    Time(int hour, int minute, int second);
 };
--- education-introduction-to-programming-cxx/src/s03-Time.cpp
+++ education-introduction-to-programming-cxx/src/s03-Time.cpp
@@ -1,80 +1,134 @@
-#include <istream>
-#include <string>
 #include <s1234/Time.h>
 
+#include <istream>
+#include <string>
+
 Time::Time(int Hour, int Minute, int Second)
-	: hour{Hour}
-	, minute{Minute}
-	, second{Second}
+        : hour{Hour}, minute{Minute}, second{Second}
 {}
 
-auto Time::to_string() const -> std::string{
+auto Time::to_string() const -> std::string
+{
+    std::string str_hour   = std::to_string(hour);
+    std::string str_minute = std::to_string(minute);
+    std::string str_second = std::to_string(second);
+    std::string zero       = "0";
 
-	std::string str_hour = std::to_string(hour);
-	std::string str_minute = std::to_string(minute);
-	std::string str_second = std::to_string(second);
-	std::string zero = "0";
+    if (hour <= 9) {
+        std::string str_hour1 = std::to_string(hour);
+        str_hour              = zero + str_hour1;
+    } else {
+        std::string str_hour = std::to_string(hour);
+    }
 
-	if(hour <= 9){
-	std::string str_hour1 = std::to_string(hour);
-	str_hour = zero + str_hour1;
-	}else{
-	std::string str_hour = std::to_string(hour);
-	}
+    if (minute <= 9) {
+        std::string str_minute1 = std::to_string(minute);
+        str_minute              = zero + str_minute1;
+    } else {
+        std::string str_minute = std::to_string(minute);
+    }
 
-	if(minute <= 9){
-	std::string str_minute1 = std::to_string(minute);
-	str_minute = zero + str_minute1;
-	}else{
-	std::string str_minute = std::to_string(minute);
-	}
+    if (second <= 9) {
+        std::string str_second1 = std::to_string(second);
+        str_second              = zero + str_second1;
+    } else {
+        std::string str_second = std::to_string(second);
+    }
 
-	if(second <= 9){
-	std::string str_second1 = std::to_string(second);
-	str_second = zero + str_second1;
-	}else{
-	std::string str_second = std::to_string(second);
-	}
-
-	return(str_hour + ":" + str_minute + ":" + str_second);
+    return (str_hour + ":" + str_minute + ":" + str_second);
 }
 
-auto Time::next_hour() -> void{
-
-	if(hour < 23){
-	hour++;
-	}else{
-	hour = 0;
-	}
+auto Time::next_hour() -> void
+{
+    if (hour < 23) {
+        hour++;
+    } else {
+        hour = 0;
+    }
 }
 
-auto Time::next_minute() -> void{
-
-	if(minute < 59){
-	minute++;
-	}else{
-	minute = 0;
-	Time::next_hour();
-	}
+auto Time::next_minute() -> void
+{
+    if (minute < 59) {
+        minute++;
+    } else {
+        minute = 0;
+        Time::next_hour();
+    }
 }
 
-auto Time::next_second() -> void{
-
-	if(second < 59){
-	second++;
-	}else{
-	second = 0;
-	Time::next_minute();
-	}
+auto Time::next_second() -> void
+{
+    if (second < 59) {
+        second++;
+    } else {
+        second = 0;
+        Time::next_minute();
+    }
 }
 
-auto main() -> int{
+auto Time::time_of_day() const -> Time_of_day
+{
+    if (hour >= 6 && hour < 10) {
+        return Time_of_day::morning;
+    } else if (hour >= 10 && hour < 18) {
+        return Time_of_day::day;
+    } else if (hour >= 18 && hour < 22) {
+        return Time_of_day::evening;
+    } else {
+        return Time_of_day::night;
+    }
+}
 
-	auto midnight = Time(23, 59, 59);
-	
-	std::cout << midnight.to_string() << std::endl;
-	midnight.next_second();
-	std::cout << midnight.to_string() << std::endl;
+auto Time::to_string(Time_of_day a) -> std::string
+{
+    // jak usunalem z nazwy funkcji 'a' i uzylem tego to program sie skompilowal
+    // ale dostalem blad 'Segmentation fault (core dumped)' zamiast pory dnia
+    // std::string str_time_of_day = Time::to_string(Time::time_of_day());
+    // return str_time_of_day;
 
-	return 0;
+    if (a == Time_of_day::morning) {
+        return "Morning";
+    } else if (a == Time_of_day::day) {
+        return "Day";
+    } else if (a == Time_of_day::evening) {
+        return "Evening";
+    } else {
+        return "Night";
+    }
+}
+
+
+auto main() -> int
+{
+    auto some_time = Time(23, 59, 59);
+
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+    some_time.next_second();
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+
+    some_time = Time(9, 59, 59);
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+    some_time.next_second();
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+
+    some_time = Time(17, 59, 59);
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+    some_time.next_second();
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+
+    some_time = Time(21, 59, 59);
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+    some_time.next_second();
+    std::cout << some_time.to_string(some_time.time_of_day()) << " : "
+              << some_time.to_string() << std::endl;
+
+    return 0;
 }

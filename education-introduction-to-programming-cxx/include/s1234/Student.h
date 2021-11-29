95300ceb930a0f4da9ce67c77f8a8b3de4faa456
Mateusz de <s24549@pjwstk.edu.pl>
2021-11-29 19:29:07 +0100

    Changes to be committed: 	modified:   Makefile 	new file:   include/s1234/Student.h 	new file:   src/s03-Student.cpp

---
+++ education-introduction-to-programming-cxx/include/s1234/Student.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+struct Student {
+
+	std::string const imie;
+	std::string const nazwisko;
+	std::string const nr_indeksu;
+	int aktualny_semestr;
+	double srednia_ocen;
+
+	auto to_string() const -> std::string;
+
+	Student(std::string imie, std::string nazwisko, std::string nr_indeksu, int aktualny_semestr, double srednia_ocen);
+
+	
+
+};
---
+++ education-introduction-to-programming-cxx/src/s03-Student.cpp
@@ -0,0 +1,24 @@
+#include <istream>
+#include <string>
+#include <s1234/Student.h>
+
+Student::Student(std::string imie, std::string nazwisko, std::string nr_indeksu, int aktualny_semestr, double srednia_ocen)
+	: imie{imie}, nazwisko{nazwisko}, nr_indeksu{nr_indeksu}, aktualny_semestr{aktualny_semestr}, srednia_ocen{srednia_ocen} {}
+
+auto Student::to_string() const -> std::string
+{
+	std::string str_aktualny_semestr = std::to_string(aktualny_semestr);
+	std::string str_srednia_ocen = std::to_string(srednia_ocen);
+
+	return (imie + ", " + nazwisko + ", " + nr_indeksu + ", sem: " + str_aktualny_semestr + ", " + str_srednia_ocen);
+
+}
+
+auto main() -> int
+{
+	auto Jan_Kowalski = Student{"Jan", "Kowalski", "s12345", 1, 4.25};
+	
+	std::cout << Jan_Kowalski.to_string() << std::endl;
+
+	return 0;
+}
